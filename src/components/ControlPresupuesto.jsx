f71503e452545af7c45c85696a55d82663e8aa20
Diego de Sierra <diegodesierra018@gmail.com>
2023-08-28 19:15:43 -0300

    first commit

---
+++ src/components/ControlPresupuesto.jsx
@@ -0,0 +1,90 @@
+import { useState, useEffect } from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+const ControlPresupuesto = ({
+  gastos,
+  setGastos,
+  presupuesto,
+  setPresupuesto,
+  setIsValidPresupuesto,
+}) => {
+  const [porcentaje, setPorcentaje] = useState(10);
+  const [disponible, setDisponible] = useState(0);
+  const [gastado, setGastado] = useState(0);
+
+  useEffect(() => {
+    const totalGastado = gastos.reduce(
+      (total, gasto) => gasto.cantidad + total,
+      0
+    );
+    const totalDisponible = presupuesto - totalGastado;
+
+    // Calcular el porcentaje gastado
+    const nuevoPorcentaje = (
+      ((presupuesto - totalDisponible) / presupuesto) *
+      100
+    ).toFixed(2);
+
+    setDisponible(totalDisponible);
+    setGastado(totalGastado);
+    setTimeout(() => {
+      setPorcentaje(nuevoPorcentaje);
+    }, 700);
+  }, [gastos]);
+
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
+
+  const handleResetApp = () => {
+    const resultado = confirm("¿Deseas reiniciar presupuesto y gastos?");
+
+    if (resultado) {
+      setGastos([]);
+      setPresupuesto(0);
+      setIsValidPresupuesto(false);
+    }
+  };
+
+  return (
+    <div className="contenedor-presupuesto contenedor sombra dos-columnas">
+      <div>
+        <CircularProgressbar
+          styles={buildStyles({
+            pathColor: porcentaje > 100 ? "#DC2626" : "#3B82F6",
+            trailColor: "#F5F5F5",
+            textColor: porcentaje > 100 ? "#DC2626" : "#3B82F6",
+          })}
+          value={porcentaje}
+          text={`${porcentaje}% Gastado`}
+        />
+      </div>
+
+      <div className="contenido-presupuesto">
+        <button className="reset-app" type="button" onClick={handleResetApp}>
+          Resetear App
+        </button>
+        <p>
+          <span>Presupuesto: </span>
+          {formatearCantidad(presupuesto)}
+        </p>
+
+        <p className={`${disponible < 0 ? "negativo" : ""}`}>
+          <span>Disponible: </span>
+          {formatearCantidad(disponible)}
+        </p>
+
+        <p>
+          <span>Gastado: </span>
+          {formatearCantidad(gastado)}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ControlPresupuesto;
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import NuevoPresupuesto from "./NuevoPresupuesto";
+import ControlPresupuesto from "./ControlPresupuesto";
+
+const Header = ({
+  gastos,
+  setGastos,
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto,
+}) => {
+  return (
+    <header>
+      <h1>Planificador de gastos</h1>
+      {isValidPresupuesto ? (
+        <ControlPresupuesto
+          presupuesto={presupuesto}
+          gastos={gastos}
+          setGastos={setGastos}
+          setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+      ) : (
+        <NuevoPresupuesto
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+      )}
+    </header>
+  );
+};
+
+export default Header;
